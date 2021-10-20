7798740d6eba7b1c6e7f754947b53ff8a6405c0f
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2021-10-20 16:06:44 +0300

    Added database creation

--- start.sh
+++ start.sh
@@ -3,6 +3,14 @@
 set -o allexport
 source .env
 
+if ! PGPASSWORD=${DATASOURCE_PASSWORD} psql -h localhost -U ${DATASOURCE_USERNAME} -lqt | cut -d \| -f 1 | grep -qw user-service ; then
+  PGPASSWORD=${DATASOURCE_PASSWORD} psql -h localhost -U ${DATASOURCE_USERNAME} -c 'create database user-service;'
+fi
+
+if ! PGPASSWORD=${DATASOURCE_PASSWORD} psql -h localhost -U ${DATASOURCE_USERNAME} -lqt | cut -d \| -f 1 | grep -qw admin-service ; then
+  PGPASSWORD=${DATASOURCE_PASSWORD} psql -h localhost -U ${DATASOURCE_USERNAME} -c 'create database admin-service;'
+fi
+
 docker-compose -f docker-compose.dev.yml up -d
 
 set +o allexport
