9ce5ca019bf5695c57688e968fbbbf325f0cfc04
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2021-10-20 19:53:26 +0300

    Fixed permissions

--- start.sh
+++ start.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+docker-compose -f docker-compose.dev.yml up -d database
+sleep 5
+
 set -o allexport
 source .env
 
 if ! PGPASSWORD=${DATASOURCE_PASSWORD} psql -h localhost -U ${DATASOURCE_USERNAME} -lqt | cut -d \| -f 1 | grep -qw user-service ; then
-  PGPASSWORD=${DATASOURCE_PASSWORD} psql -h localhost -U ${DATASOURCE_USERNAME} -c 'create database user-service;'
+  PGPASSWORD=${DATASOURCE_PASSWORD} psql -h localhost -U ${DATASOURCE_USERNAME} -c 'create database "user-service";'
 fi
 
 if ! PGPASSWORD=${DATASOURCE_PASSWORD} psql -h localhost -U ${DATASOURCE_USERNAME} -lqt | cut -d \| -f 1 | grep -qw admin-service ; then
-  PGPASSWORD=${DATASOURCE_PASSWORD} psql -h localhost -U ${DATASOURCE_USERNAME} -c 'create database admin-service;'
+  PGPASSWORD=${DATASOURCE_PASSWORD} psql -h localhost -U ${DATASOURCE_USERNAME} -c 'create database "admin-service";'
 fi
 
-docker-compose -f docker-compose.dev.yml up -d
-
 set +o allexport
+
+docker-compose -f docker-compose.dev.yml up -d
